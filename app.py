a366830f1ba14e7fb77eb16075efa273c2552622
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-17 09:52:29 -0500

    test routes, connect to local host

--- app.py
+++ app.py
@@ -2,13 +2,41 @@ from flask import Flask, jsonify, request
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
-db = PostgresqlDatabase('people', user='postgres', password='', host='localhost', port=5432)
+# db = PostgresqlDatabase('people', user='laurenpowers',
+#                         password='12345', host='localhost', port=5432)
 
-class BaseModel(Model):
-  class Meta:
-    database = db
-    
-class Pasta(BaseModel):
-  name = CharField()
-  description = CharField()
+
+# class BaseModel(Model):
+#     class Meta:
+#         database = db
+
+
+# class Pasta(BaseModel):
+#     name = CharField()
+#     description = CharField()
+
+
+# db.connect()
+# db.drop_tables([Pasta])
+# db.create_tables([Pasta])
+
+# Pasta(name='Farfalle', description='bow tie shaped').save()
+# Pasta(name='Fusilli', description='cork screw shaped').save()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, world!"
   
+@app.route('/get-json')
+def get_json():
+  return jsonify({
+    "name": "Garfield",
+    "hatesMondays": True,
+    "friends": ["Sheldon", "Wade", "Orson", "Squeak"]
+  })
+
+
+app.run(debug=True, port=5000)
