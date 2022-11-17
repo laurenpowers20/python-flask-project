7b69dad0c90e83f7ed19b8e1871d054271c6abaa
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-17 10:45:51 -0500

    start adding routes

--- app.py
+++ app.py
@@ -2,41 +2,37 @@ from flask import Flask, jsonify, request
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
-# db = PostgresqlDatabase('people', user='laurenpowers',
-#                         password='12345', host='localhost', port=5432)
+db = PostgresqlDatabase('pasta', user='laurenpowers',
+                        password='12345', host='localhost', port=5432)
 
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-# class BaseModel(Model):
-#     class Meta:
-#         database = db
+class Pasta(BaseModel):
+    name = CharField()
+    description = CharField()
 
+db.connect()
+db.drop_tables([Pasta])
+db.create_tables([Pasta])
 
-# class Pasta(BaseModel):
-#     name = CharField()
-#     description = CharField()
-
-
-# db.connect()
-# db.drop_tables([Pasta])
-# db.create_tables([Pasta])
-
-# Pasta(name='Farfalle', description='bow tie shaped').save()
-# Pasta(name='Fusilli', description='cork screw shaped').save()
+Pasta(name='Farfalle', description='bow tie shaped').save()
+Pasta(name='Fusilli', description='cork screw shaped').save()
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return "Hello, world!"
-  
-@app.route('/get-json')
-def get_json():
-  return jsonify({
-    "name": "Garfield",
-    "hatesMondays": True,
-    "friends": ["Sheldon", "Wade", "Orson", "Squeak"]
-  })
-
+@app.route('/pasta/', methods=['GET', 'POST'])
+@app.route('/pasta/<id>', methods=['GET', 'PUT', 'DELETE'])
+def endpoint(id=None):
+  if request.method == 'GET':
+    if id:
+        return jsonify(model_to_dict(Pasta.get(Pasta.id == id)))
+    else:
+        pasta_list = []
+        for pasta in Pasta.select():
+            pasta_list.append(model_to_dict(pasta))
+        return jsonify(pasta_list)
+    
 
 app.run(debug=True, port=5000)
